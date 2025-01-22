90aee749781c4cbf5e04f974e4164e8ae1606521
pramit_sr <mepramitsarkarr@gmail.com>
2025-01-23 01:51:37 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Add alias for mongoose to ensure correct version is used
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      mongoose: 'mongoose',
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
